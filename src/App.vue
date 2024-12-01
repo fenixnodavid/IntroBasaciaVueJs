af92b744bbf40238910c88884261e8fc1d7c31ae
David Rueda <yamilrueda88@gmail.com>
2024-11-30 22:31:10 -0300

    proyectos vue
    commit de proyectos

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
 import Educacion from './components/EducacionComponente.vue';
 import ExperienciaComponente from './components/ExperienciaComponente.vue';
+import ProyectosComponente from './components/ProyectosComponente.vue';
 </script>
 
 <template>
@@ -11,19 +12,26 @@ import ExperienciaComponente from './components/ExperienciaComponente.vue';
     <NavBar/>
     <!--Componente Datos Personales-->
     <DatosPersonales/>
-    
-  
-
 </header>
 <main>
-    <section id="educacion"><h2>Educación - Cursos</h2>
-        <!-- Linea Del Tiempo-->
-    <Educacion/>
+    <section id="educacion">
+        <h2>Educación - Cursos</h2>
+        <!-- Linea Del Tiempo - Componente Educacion-->
+        <Educacion/>
     </section>
-    <section id="experiencia"><h2>Experiencia</h2>
-    <ExperienciaComponente/>
+    <section id="experiencia">
+        <h2>Experiencia</h2>
+        <!--Componente Experiencia-->
+        <ExperienciaComponente/>
     </section>
-    <section id="proyectos"><h2>Proyectos</h2></section>
+    <section id="proyectos">
+        <h2>Proyectos</h2>
+        <!--Compoenten Proyectos-->
+        <ProyectosComponente/>
+    </section>
+
+    <!-- Componente de Proyectos-->
+
     <section id="habilidades"><h2>Habilidades</h2></section>
     <section id="intereses"><h2>Intereses</h2></section>
 </main>
---
+++ src/components/ProyectosComponente.vue
@@ -0,0 +1,88 @@
+<script setup>
+// Este componente muestra una galeria de proyectos coninformacion sobre cada proyecto
+import { ref } from 'vue';
+// .ref es una funcion que se utiliza para crear una referencia reactiva en Vue 3
+var misProyectos = ref([]);
+// .value es una propiedad que se utiliza para acceder al valor de una referencia reactiva en Vue 3
+misProyectos.value = ([
+// esto es un array de objetos que contiene informacion sobre los proyectos
+{
+    id: 1,
+    src:"src/assets/EL MAGO (Ver. Mitologico).jpeg",
+    titulo: "Aplicacion para Lectura de Tarot",
+    descripcion: "Cree una aplicacion para celular, donde érmite conocer los arcanos y las demas cartas aprender a tirar las cartas",
+    projectoLink: "https://www.instagram.com/institutozen/?hl=es",
+    githubLink: "https://github.com/fenixnodavid/IntroBasaciaVueJs"
+},
+{
+    
+}
+])
+
+</script>
+
+<template>
+    <div class="galeria">
+        <div class="proyecto">
+            <img src="src/assets/EL MAGO (Ver. Mitologico).jpeg" alt="Proyecto 1">
+            </div class="proyecto-info">        
+                <h3>Tarot</h3>
+                <p>Cree una app sobre lectura de tarot</p>
+                <div class="proyecto-links">
+                    <a href="https://www.instagram.com/institutozen/?hl=es" class=" btn-ver-mas" target="_blank" rel="noopener noreferrer">ver 
+                        Proyecto</a>
+                    <a href="https://github.com/fenixnodavid/IntroBasaciaVueJs" class="github-link" target="_blank"
+                        rel="noopener noreferrer">ver Codigo en GitHub</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+/*Estilos Fondo Animado de Css de la galeria*/
+/*La clase 'galeria' es el contenedor principal de la galeria de proyectos */
+.galeria{
+/* Establece el ancho de la galeria al 100% del contenedor padre */ 
+    width: 100%;
+    /* Establece la altura de la galeria al 100% del contenedor padre */
+    height: 100;
+    /* Utiliza flebox para organizar los elementos hijos en la galeria */
+    display: flex;
+    /* permite que los elemntos se ajuusten y pasen a la siguiente linea si no caben en una sola fila */
+    flex-wrap: wrap;
+    /* Establece un espacio de 20px entre los elementos de la galeria */
+    gap: 20px;
+     /* agrega un padding de 20px alrededor de la galeria */
+    padding: 20px;
+    /* Centra los elmentos hijos horizontalmente */
+    justify-content: center;
+    /* Aplica un fondo con un degradado de colores */
+    background: linear-gradient(-45deg,#021526, #0334E6, #6EACDA, #E2E286);
+
+    /* background-image: url('/src/assets/fondo-proyectos.jpg'); */
+    /* Ajusta el tamaño del fondo para que cubra el area complea */
+    background-size: 400% 400%;
+    /* Aplica un aanimacion al fondo que dura 15 segundos, tiene una transicion suave y se repite indefinidamente */
+    animation: gradiaent 15s ease infinite;
+
+    /* background-position: center;
+    background-repeat: no-repeat; */
+}
+ /*Defin una animacion llada 'gradient' para cambiar la posicion del fondo */
+ @keyframes gradient {
+    /* Al inicio. el fondo estaposicionado en el 0% horizontal y 20% vertical */
+    0% {
+        background-position: 0% 50%;
+    }
+    50% {
+        background-position: 100% 50%;
+    }
+    100% {
+        background-position: 0% 50%;
+    }
+
+};
+
+
+</style>
